22a991f0ee4fd5922bd0286ce8d9c8e7041c7995
Ryan Mukai <ryanmukai@pop-os.localdomain>
2023-04-29 23:35:53 -0700

    Current code

--- test/test_chatgpt4_multi_agent_2.py
+++ test/test_chatgpt4_multi_agent_2.py
@@ -1,126 +1,116 @@
+import random
 import openai
 from config.config import *
+
 openai.api_key = config_json['openai_api_key']
 
 class Agent:
-    def __init__(self, agent_id, num_agents):
-        self.id = agent_id
-        self.num_agents = num_agents
-        self.messages = []
-        self.addressed_to = None
-        
-        if self.num_agents > 1:
-            self.addressing_instructions = f"Please address your messages to 'all' or to a specific agent number between 0 and {self.num_agents - 1}, or to 'Human' (e.g. 'ask agent1 ...', 'send agent2 ...', 'ask_Human ...', or 'send_Human ...')"
+    def __init__(self, agent_id):
+        self.agent_id = agent_id
+        self.knowledge = []
+        self.memory = []
+        self.problem_solved = False
+        self.failed = False
+
+    def check_solution(self, message):
+        if "I know the answer: " in message:
+            self.problem_solved = True
+        elif "Insufficient information." in message and len(self.memory) >= 10:
+            self.failed = True
+        return message
+
+    def receive_message(self, sender_id, message, broadcast=False):
+        self.memory.append((sender_id, message, broadcast))
+        response = self.generate_response(message)
+        response = self.check_solution(response)
+
+        if self.problem_solved or self.failed:
+            return None
+
+        if "Agent " in response:
+            receiver_id = int(response.split("Agent ")[1].split(",")[0])
+            return (self.agent_id, response, False, receiver_id)
         else:
-            self.addressing_instructions = "You are the only agent in this environment. Please address your messages to 'all' or to 'Human'."
-        
-        self.add_message("assistant", self.addressing_instructions)
-    
-    def add_message(self, role, content):
-        self.messages.append({"role": role, "content": content})
-        
-    def send_message(self, addressed_to, content):
-        addressed_to.add_message(self.id, content)
-    
-    def generate_response(self, prompt, addressed_to=None):
-        messages = self.messages.copy()
-        messages.append({"role": "user", "content": str(prompt)})
-        if addressed_to is not None:
-            addressed_to_str = addressed_to.id if addressed_to.id == "Human" else f"agent{addressed_to.id}"
-            self.add_message("assistant", f"Sending message to {addressed_to_str}")
-        else:
-            self.add_message("assistant", "Broadcasting message to all agents")
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=messages,
-            temperature=0.7,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            timeout=10,
+            return (self.agent_id, response, True)
+
+    def update_knowledge(self, statement):
+        self.knowledge.append(statement)
+
+    def generate_response(self, message):
+        user_instruction = f"{message} If you know the answer, say 'I know the answer: '. If you need to ask another agent, say 'Agent X, please help: '. If there is not enough information to solve the problem, say 'Insufficient information.'."
+
+        messages = [{"role": "system", "content": "You are a helpful assistant."}]
+        for sender_id, msg, _ in self.memory:
+            role = "user" if sender_id != self.agent_id else "assistant"
+            messages.append({"role": role, "content": msg})
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages,
+            user_prompt=user_instruction
         )
-        text = response.choices[0].text.strip()
-        self.add_message("assistant", text)
-        return text
-    
-class MultiAgentEnvironment:
-    def __init__(self, num_agents):
-        self.agents = [Agent(i, num_agents) for i in range(num_agents)]
-        self.agents.append(Agent("Human", num_agents))
-    
-    def get_agent(self, agent_id):
+
+        return completion.choices[0].message['content']
+
+class Environment:
+    def __init__(self, n_agents):
+        self.agents = [Agent(agent_id) for agent_id in range(n_agents)]
+
+    def print_conversation(self):
+        print("Conversation:")
         for agent in self.agents:
-            if agent.id == agent_id:
-                return agent
-        return None
-    
-    def parse_message(self, sender_id, message):
-        tokens = message.split(" ")
-        if tokens[0] == "ask":
-            addressed_to_str = tokens[1]
-            if addressed_to_str == "all":
-                for agent in self.agents:
-                    if agent.id != sender_id:
-                        agent.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
-            elif addressed_to_str == "Human":
-                addressed_to = self.get_agent("Human")
-                addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
-            elif addressed_to_str.startswith("agent") and addressed_to_str[5:].isdigit():
-                addressed_to_id = int(addressed_to_str[5:])
-                addressed_to = self.get_agent(addressed_to_id)
-                if addressed_to is None:
-                    self.get_agent("Human").add_message("assistant", f"No agent with id {addressed_to_id}")
-                    return
-                addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
+            for sender_id, msg, broadcast in agent.memory:
+                sender = f"Agent {sender_id}"
+                print(f"{sender}: {msg}")
+
+    def send_message(self, sender_id, message, broadcast, receiver_id=None):
+        print(f"Agent {sender_id}: {message}")
+
+        if broadcast:
+            for agent in self.agents:
+                if agent.agent_id != sender_id:
+                    response = agent.receive_message(sender_id, message, broadcast=True)
+                    if response is not None:
+                        self.send_message(*response)
+                    else:
+                        break
+        else:
+            response = self.agents[receiver_id].receive_message(sender_id, message)
+            if response is not None:
+                self.send_message(*response)
             else:
-                self.get_agent("Human").add_message("assistant", f"Invalid agent id: {addressed_to_str}")
                 return
-        elif tokens[0] == "send":
-            addressed_to_str = tokens[1]
-            if addressed_to_str == "all":
-                for agent in self.agents:
-                    if agent.id != sender_id:
-                        agent.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
-                addressed_to = self.get_agent("Human")
-                addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
-            elif addressed_to_str == "Human":
-                addressed_to = self.get_agent("Human")
-                addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
-            elif addressed_to_str.startswith("agent") and addressed_to_str[5:].isdigit():
-                addressed_to_id = int(addressed_to_str[5:])
-                addressed_to = self.get_agent(addressed_to_id)
-                if addressed_to is None:
-                    self.get_agent("Human").add_message("assistant", f"No agent with id {addressed_to_id}")
-                    return
-                addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[2:]))
-            else:
-                self.get_agent("Human").add_message("assistant", f"Invalid agent id: {addressed_to_str}")
-                return
-        elif tokens[0] == "ask_Human":
-            addressed_to = self.get_agent("Human")
-            addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[1:]))
-        elif tokens[0] == "send_Human":
-            addressed_to = self.get_agent("Human")
-            addressed_to.send_message(self.get_agent(sender_id), " ".join(tokens[1:]))
+            
+    def provide_problem_statement(self, problem_statement):
+        for agent in self.agents:
+            agent.update_knowledge(problem_statement)
 
+    def provide_agent_info(self, agent_id, info):
+        self.agents[agent_id].update_knowledge(info)
 
-env = MultiAgentEnvironment(num_agents=2)
+    def start_conversation(self):
+        random_agent_id = random.randint(0, len(self.agents) - 1)
+        self.send_message(random_agent_id, self.agents[random_agent_id].knowledge[-1], broadcast=True)
 
-# Initialize agents with necessary knowledge
-env.parse_message(0, "send Human Agent 0 knows that Igor is dating Natasha.")
-env.parse_message(0, "send Human Natasha likes roses and orchids but hates carnations.")
-env.parse_message(1, "send Human Agent 1 knows that the florist has orchids and carnations but not roses.")
+env = Environment(2)
+problem_statement = "Tell Igor what flowers to purchase for Natasha."
+env.provide_problem_statement(problem_statement)
 
-# Agent 0 asks Agent 1 what flowers are available and what Natasha likes
-env.parse_message(0, "ask Agent1 What flowers does the florist have?")
-env.parse_message(1, "send Agent0 The florist has orchids and carnations.")
-env.parse_message(0, "ask Agent1 Which flowers does Natasha like?")
-env.parse_message(1, "send Agent0 Natasha likes roses and orchids but hates carnations.")
+agent0_info = "Natasha likes roses and orchids but not carnations. Igor does not know which flowers only available florist has."
+agent1_info = "The only available florist has orchids and carnations but not roses."
+env.provide_agent_info(0, agent0_info)
+env.provide_agent_info(1, agent1_info)
 
-# Agent 0 determines which flowers Igor should purchase
-env.parse_message(0, "ask Agent1 Does the florist have roses?")
-env.parse_message(1, "send Agent0 No, the florist does not have roses.")
-env.parse_message(0, "send Agent1 Okay, got it. Tell Igor to get orchids.")
+env.start_conversation()
 
-# Agent 1 broadcasts the final message to Igor
-env.parse_message(1, "send Human Igor, please purchase orchids from the florist.")
+solution = ""
+for agent in env.agents:
+    if agent.problem_solved:
+        print(f"Agent {agent.agent_id} has solved the problem!")
+        solution = agent.memory[-1][1]
+    elif agent.failed:
+        print(f"Agent {agent.agent_id} has failed to solve the problem.")
+
+env.print_conversation()
+print(f"Solution: {solution}")
+
